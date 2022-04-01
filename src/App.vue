5292d7f53739e0c37ca5aee58e78f40a9ac71525
Lineez <qewqew26@gmail.com>
2022-04-01 12:10:07 +0300

    default up

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-    <div></div>
+    <header></header>
+    <main class="default-theme"></main>
+    <footer></footer>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {};
+    },
+    methods: {},
+};
 </script>
 
 <style lang="scss">
