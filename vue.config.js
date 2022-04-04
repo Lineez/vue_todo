6e91542512dbafb867555fb404a15bb9169e0f9f
Lineez <qewqew26@gmail.com>
2022-04-04 11:57:03 +0300

    config up

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,9 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                additionalData: `@import "@/assets/scss/vue_todo/_mixin.scss";`,
+                additionalData: `@import "@/assets/scss/default/_mixin.scss";`,
             },
         },
     },
+    publicPath: process.env.NODE_ENV === "production" ? "/vue_todo/" : "/",
 };
